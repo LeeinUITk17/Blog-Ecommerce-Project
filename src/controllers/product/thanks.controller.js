b8b8514f3111ed577536b2d1a02b33d65c344a2a
Lê Thanh Tài <mrtaivietbac@gmail.com>
2024-03-17 14:26:19 +0700

    update config cookie

--- src/controllers/product/thanks.controller.js
+++ src/controllers/product/thanks.controller.js
@@ -1,6 +1,9 @@
 const{
     addItem,
 }=require('../../services/bill.service');
+const jwt = require('jsonwebtoken');
+const jwtHelper = require('../../helper/jwt.helper');
+const {  generateToken } = jwtHelper;
 class thanksController {
     getAll = async (req, res, next) => {
         res.render('product/thanks');
@@ -8,7 +11,20 @@ class thanksController {
     add=async(req,res,next)=>{
        console.log(req.body);
        await addItem(req.body);
-        res.redirect('/shop/thanks');
+       const token = req.cookies.token;
+if (token) {
+    try {
+        const decodedToken = jwt.verify(token, 'cnttvietnhatk17');
+        const updatedProductIds = []; 
+        const updatedToken = generateToken(decodedToken.userId, decodedToken.username, updatedProductIds);
+        res.cookie('token', updatedToken);
+    } catch (error) {
+        console.error('JWT Verification Error:', error);
+        return res.status(500).send('Internal Server Error');
+    }
+}
+
+       return res.redirect('/shop/thanks');
       }
 }
 
