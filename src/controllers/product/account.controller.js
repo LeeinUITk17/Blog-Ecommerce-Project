38c315cfce1b30853a1ec75eade42c718126d7c9
Lê Thanh Tài <mrtaivietbac@gmail.com>
2024-04-03 21:20:52 +0700

    update config profile user

--- src/controllers/product/account.controller.js
+++ src/controllers/product/account.controller.js
@@ -1,7 +1,24 @@
+const {
+    updateInformation,
+    getItemById,
+}=require('../../services/user.service');
 class accountController {
     getAll = async (req, res, next) => {
-        const user=req.user;
-        user?res.render('product/account',{user}):res.redirect('/shop/login'); 
+       // const user=req.user;
+        const auth= await getItemById(req.user._id);
+        console.log(auth);
+        auth?res.render('product/account',{auth}):res.redirect('/shop/login'); 
+    }
+    updateInformation=async(req,res,next)=>{
+        const id=req.user._id;
+        console.log(req.body);
+        try {
+        await updateInformation(id,req.body);
+            res.redirect('/shop/account');
+        } catch (error) {
+            console.log('update fail'+ error);
+            res.redirect('/shop/account');
+        }
     }
 }
 
--- src/routes/product/account/index.js
+++ src/routes/product/account/index.js
@@ -2,4 +2,5 @@ const express = require('express');
 const accountController = require('../../../controllers/product/account.controller');
 const router = express.Router();
 router.get('/',accountController.getAll);
+router.post('/',accountController.updateInformation);
 module.exports = router;
